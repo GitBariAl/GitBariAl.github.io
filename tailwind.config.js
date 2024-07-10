6ce197df7f95a040444f092c99e5d9651d049b69
GitBariAl <91183150+GitBariAl@users.noreply.github.com>
2024-07-10 15:10:41 +0200

    messing around with tailw and daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    '*.{html,js}',
-  ],
+  content: ["*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [require('daisyui')],
-}
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#1d4ed8",
 
+          secondary: "#22d3ee",
+
+          accent: "#fde047",
+
+          neutral: "#1f2937",
+
+          "base-100": "#f5f5f4",
+
+          info: "#67e8f9",
+
+          success: "#4ade80",
+
+          warning: "#fb923c",
+
+          error: "#dc2626",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
