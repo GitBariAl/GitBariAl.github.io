2fec15f1b379b7eced7772d12bc21c7b5f392090
GitBariAl <91183150+GitBariAl@users.noreply.github.com>
2024-07-12 10:43:19 +0200

    enriching my page

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,6 +25,17 @@ module.exports = {
 
           error: "#dc2626",
         },
+        darkTheme: {
+          primary: "#1d4ed8", // Blu, può rimanere uguale o leggermente più chiaro per il tema scuro
+          secondary: "#22d3ee", // Ciano, può rimanere uguale o leggermente più chiaro per il tema scuro
+          accent: "#fde047", // Giallo, può rimanere uguale poiché fornisce un buon contrasto anche su sfondi scuri
+          neutral: "#2a303c", // Grigio più chiaro per il testo e gli elementi su sfondo scuro
+          "base-100": "#1f2937", // Colore di sfondo principale più scuro
+          info: "#3dd0f3", // Blu più chiaro per il tema scuro
+          success: "#22c55e", // Verde, può rimanere uguale o diventare leggermente più chiaro
+          warning: "#f97316", // Arancione, può rimanere uguale poiché fornisce un buon contrasto
+          error: "#ef4444", // Rosso, può rimanere uguale o diventare leggermente più chiaro
+        },
       },
     ],
   },
