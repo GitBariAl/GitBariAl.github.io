30cb56ef9446770135d2403724974ceb249140ab
GitBariAl <91183150+GitBariAl@users.noreply.github.com>
2024-09-06 15:34:45 +0200

    create branch

---
+++ js/index.js
@@ -0,0 +1,9 @@
+/*
+//drawer mechanism
+document.querySelector('a[href="#footer_info"]').addEventListener("click", function () {
+    // Seleziona l'input checkbox che controlla il drawer
+    const drawerCheckbox = document.getElementById("my-drawer");
+    // Imposta il suo stato su false per chiudere il drawer
+    drawerCheckbox.checked = false;
+});
+*/
