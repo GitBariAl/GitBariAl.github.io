cc81279857a77eb1d5cdf5393ff1db6e7ab21281
GitBariAl <91183150+GitBariAl@users.noreply.github.com>
2024-09-14 15:36:35 +0200

    feat(esthetic): save theme mecanichs added along with little change in elements distance

--- js/index.js
+++ js/index.js
@@ -1,4 +1,20 @@
 
+//theme toggler (save and reuse theme saved in local storage)
+document.addEventListener('DOMContentLoaded', function () {
+  const themeToggler = document.getElementById('theme-toggler');
+  const currentTheme = localStorage.getItem('theme') || 'lightTheme';
+
+  // Applica il tema salvato
+  document.documentElement.setAttribute('data-theme', currentTheme);
+  themeToggler.checked = currentTheme === 'darkTheme';
+
+  // Aggiungi l'evento di cambio tema
+  themeToggler.addEventListener('change', function () {
+    const theme = themeToggler.checked ? 'darkTheme' : 'lightTheme';
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  });
+});
 
 document.addEventListener('scroll', function () {
   const containers = document.querySelectorAll('.grid-item'); //careful with this selector, must be unique
@@ -17,7 +33,7 @@ document.addEventListener('scroll', function () {
       animatedElement.classList.remove('top-1/2', '-translate-y-1/2');
       animatedElement.style.marginTop = '1rem'; // Aggiungi margine superiore di 0.5rem all'elemento animato
       heroes.forEach((hero, index) => {
-          setTimeout(() => {
+        setTimeout(() => {
           hero.style.marginTop = `${animatedElementHeight + 22}px`; // Aggiungi margine superiore agli hero
           hero.classList.remove('opacity-0', 'translate-y-4');
           hero.classList.add('opacity-100', 'translate-y-0');
